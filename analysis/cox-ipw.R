64addb86ea5d8bbc26a72991a980e5a62ee856a4
Tom Palmer <remlapmot@hotmail.com>
2022-12-05 13:50:01 +0000

    Bump version number

--- analysis/cox-ipw.R
+++ analysis/cox-ipw.R
@@ -447,7 +447,7 @@ if (sum(episode_info[episode_info$time_period != "days_pre", ]$N_events) < total
 # Save output ------------------------------------------------------------------
 print("Save output")
 
-results$cox_ipw <- "v0.0.13"
+results$cox_ipw <- "v0.0.14"
 
 write.csv(results,
           file = paste0("output/", opt$df_output),
