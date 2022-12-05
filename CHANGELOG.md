b7c4ba8ac1f7760d66960997fac3fac4b50a083a
Tom Palmer <remlapmot@hotmail.com>
2022-12-05 18:35:02 +0000

    Update mathieudutour/github-tag-action GHA to v6.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# [v0.0.15](https://github.com/opensafely-actions/cox-ipw/releases/tag/v0.0.15)
+
+- Update `mathieudutour/github-tag-action` GitHub Action to v6.1 tag
+
 # [v0.0.14](https://github.com/opensafely-actions/cox-ipw/releases/tag/v0.0.14)
 
 - Fix typo that caused fatal error for fn-check_covariates.R
--- analysis/cox-ipw.R
+++ analysis/cox-ipw.R
@@ -447,7 +447,7 @@ if (sum(episode_info[episode_info$time_period != "days_pre", ]$N_events) < total
 # Save output ------------------------------------------------------------------
 print("Save output")
 
-results$cox_ipw <- "v0.0.14"
+results$cox_ipw <- "v0.0.15"
 
 write.csv(results,
           file = paste0("output/", opt$df_output),
