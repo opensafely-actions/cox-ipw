d8490bc52cd8ddb6468442e46401777c593d084a
Tom Palmer <remlapmot@hotmail.com>
2025-10-17 08:29:10 -0300

    Add note about single covariate edge case

--- analysis/cox-ipw.R
+++ analysis/cox-ipw.R
@@ -70,7 +70,7 @@ option_list <- list(
     "--covariate_other",
     type = "character",
     default = "cov_cat_ethnicity;cov_num_consulation_rate;cov_bin_healthcare_worker;cov_bin_carehome_status",
-    help = "Semi-colon separated list of other covariates or filename of text file containing semi-colon separated list of other covariates to be included in the regression model; specify argument as NULL to run age, age squared, sex adjusted model only [default %default]",
+    help = "Semi-colon separated list of other covariates or filename of text file containing semi-colon separated list of other covariates to be included in the regression model; specify argument as NULL to run age, age squared, sex adjusted model only. Note that if you are including only a single covariate please include the semi-colon after it [default %default]",
     metavar = "varname_1;varname_2;... or covariate-other.txt"
   ),
   make_option(
